b5fbeef3ee325bdd2ed0b70725f14b3341d8f0a5
steandean <61210469+steandean@users.noreply.github.com>
2021-01-28 12:13:55 +0000

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 password_provided = input("Please enter a password: ")  # This is input in the form of a string
 password = password_provided.encode()  # Convert to type bytes
-salt =  b'\xc7d\xb4`\xebK\x14\xe7z\x8fQo\xa0\xf8\xb7\xe8\xec#n\xaf\xaf!_\xff\xfeZ\xa1H\x80W\x08\xf4' # a key from os.urandom(32) of type bytes
+salt =  b'' # insert a key here. The key should be from os.urandom(32) of type bytes
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
     length=32,
